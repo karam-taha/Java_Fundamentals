dfa9392bdf3f2fd7dfa0f3cced5d660828a71cd4
Karam Taha <karamtaha15@gmail.com>
2022-10-30 15:53:33 +0200

    first assignment

---
+++ first_assignment/first_assignment.java
@@ -0,0 +1,7 @@
+public class first_assignment {
+    public static void main(String[] args) {
+        System.out.println("My name is Karam Taha");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Surda, PS");
+    }
+}
