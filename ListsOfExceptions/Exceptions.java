a047aaa1e1cf3fa7351134b2d2bc85afb73140d9
Karam Taha <karamtaha15@gmail.com>
2022-10-30 16:01:39 +0200

    seventh assignment in java fundamentals

---
+++ ListsOfExceptions/Exceptions.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Exceptions{
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i = 0; i < myList.size(); i++) {
+            try{
+            Integer castedValue = (Integer) myList.get(i);
+            System.out.println(castedValue);
+            } catch (Exception e){
+            System.out.println("error");
+            // back-up plan here.
+            }
+        }
+    }
+}
+
